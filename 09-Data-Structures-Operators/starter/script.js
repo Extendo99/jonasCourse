14a49d4dcc2837ecf0e6adb48ff1a256a6e9680f
Extendo99 <konrad.lada1999@gmail.com>
2024-12-02 11:52:50 +0100

    Rest

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -271,7 +271,60 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+// Rest Operator///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 1) Destructuring
+// Arrays
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
+
+// Objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+// 2) Functions
+// const add = function (...numbers) {
+//   console.log(numbers);
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1, 4);
+
+// Spread
+// const x = [23, 5, 7];
+// add(...x);
+
+// Function OrderPizza
+// restaurant.orderPizza(`Mushrooms`, `onion`, `olives`, `spinach`);
+// restaurant.orderPizza(`Mushrooms`);
+
+// Assigment #4
+// 4.1
+// const [mainKeyword, ...rest] = books[0].keywords;
+
+// 4.2
+// const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+
+// 4.3
+// function printBookAuthorsCount(title, ...authors) {
+//   console.log(`The book "${title}" has ${authors.length} authors`);
+// }
 
 // Spread Operator//////////////////////////////////////////////////////////////////////////////////////////////////
 // const arr = [7, 8, 9];
