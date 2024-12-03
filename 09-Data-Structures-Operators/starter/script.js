a8d0186aa1952d410df2f33cdf83bd5bca91a4ed
Extendo99 <konrad.lada1999@gmail.com>
2024-12-03 11:49:30 +0100

    Enchanced

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -231,28 +231,34 @@ const books = [
 ];
 
 // Restaurant Array ///////////////////////////////////////////////////////////////////////////////////////////////////
+const weekdays = [`mon`, `tue`, `wed`, `thu`, `fri`, `sat`, `sun`];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 
-  order: function (starterIndex, mainIndex) {
+  // ES6 Enchanced Object Literals
+  openingHours,
+  // Without Function Keyword
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -276,6 +282,29 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+// Assigment #9////////////////////////////////////////////////////////////////////////////////////////////////////////
+//8.1
+// const bookData = [
+//   ["title", "Computer Networking: A Top-Down Approach"],
+//   ["author", ["James F. Kurose", "Keith W. Ross"]],
+//   ["publisher", "Addison Wesley"],
+// ];
+
+// const newBook = {
+//   [bookData[0][0]]: bookData[0][1],
+//   [bookData[1][0]]: bookData[1][1],
+//   [bookData[2][0]]: bookData[2][1],
+// };
+
+// //8.2
+// const pages = 880;
+
+// const newBook2 = {
+//   title: "The C Programming Language",
+//   author: ["Brian W. Kernighan", "Dennis M. Ritchie"],
+//   pages,
+// };
 
 // For Looping Arrays//////////////////////////////////////////////////////////////////////////////////////////////////
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
