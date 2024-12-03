c46fcd6a86c2630ac6d0c64994396545d914bad7
Extendo99 <konrad.lada1999@gmail.com>
2024-12-03 09:31:44 +0100

    Challenge #2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,281 +1,392 @@
-"use strict";
+// "use strict";
 
 // Data needed for a later exercise
 // const flights =
 //   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Assigment Array Book //////////////////
-const books = [
-  {
-    title: "Algorithms",
-    author: ["Robert Sedgewick", "Kevin Wayne"],
-    publisher: "Addison-Wesley Professional",
-    publicationDate: "2011-03-24",
-    edition: 4,
-    keywords: [
-      "computer science",
-      "programming",
-      "algorithms",
-      "data structures",
-      "java",
-      "math",
-      "software",
-      "engineering",
-    ],
-    pages: 976,
-    format: "hardcover",
-    ISBN: "9780321573513",
-    language: "English",
-    programmingLanguage: "Java",
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.41,
-        ratingsCount: 1733,
-        reviewsCount: 63,
-        fiveStarRatingCount: 976,
-        oneStarRatingCount: 13,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: "Structure and Interpretation of Computer Programs",
-    author: [
-      "Harold Abelson",
-      "Gerald Jay Sussman",
-      "Julie Sussman (Contributor)",
-    ],
-    publisher: "The MIT Press",
-    publicationDate: "2022-04-12",
-    edition: 2,
-    keywords: [
-      "computer science",
-      "programming",
-      "javascript",
-      "software",
-      "engineering",
-    ],
-    pages: 640,
-    format: "paperback",
-    ISBN: "9780262543231",
-    language: "English",
-    programmingLanguage: "JavaScript",
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.36,
-        ratingsCount: 14,
-        reviewsCount: 3,
-        fiveStarRatingCount: 8,
-        oneStarRatingCount: 0,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: "Computer Systems: A Programmer's Perspective",
-    author: ["Randal E. Bryant", "David Richard O'Hallaron"],
-    publisher: "Prentice Hall",
-    publicationDate: "2002-01-01",
-    edition: 1,
-    keywords: [
-      "computer science",
-      "computer systems",
-      "programming",
-      "software",
-      "C",
-      "engineering",
-    ],
-    pages: 978,
-    format: "hardcover",
-    ISBN: "9780130340740",
-    language: "English",
-    programmingLanguage: "C",
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 4.44,
-        ratingsCount: 1010,
-        reviewsCount: 57,
-        fiveStarRatingCount: 638,
-        oneStarRatingCount: 16,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: "Operating System Concepts",
-    author: ["Abraham Silberschatz", "Peter B. Galvin", "Greg Gagne"],
-    publisher: "John Wiley & Sons",
-    publicationDate: "2004-12-14",
-    edition: 10,
-    keywords: [
-      "computer science",
-      "operating systems",
-      "programming",
-      "software",
-      "C",
-      "Java",
-      "engineering",
-    ],
-    pages: 921,
-    format: "hardcover",
-    ISBN: "9780471694663",
-    language: "English",
-    programmingLanguage: "C, Java",
-    onlineContent: false,
-    thirdParty: {
-      goodreads: {
-        rating: 3.9,
-        ratingsCount: 2131,
-        reviewsCount: 114,
-        fiveStarRatingCount: 728,
-        oneStarRatingCount: 65,
-      },
-    },
-  },
-  {
-    title: "Engineering Mathematics",
-    author: ["K.A. Stroud", "Dexter J. Booth"],
-    publisher: "Palgrave",
-    publicationDate: "2007-01-01",
-    edition: 14,
-    keywords: ["mathematics", "engineering"],
-    pages: 1288,
-    format: "paperback",
-    ISBN: "9781403942463",
-    language: "English",
-    programmingLanguage: null,
-    onlineContent: true,
-    thirdParty: {
-      goodreads: {
-        rating: 4.35,
-        ratingsCount: 370,
-        reviewsCount: 18,
-        fiveStarRatingCount: 211,
-        oneStarRatingCount: 6,
-      },
-    },
-    highlighted: true,
-  },
-  {
-    title: "The Personal MBA: Master the Art of Business",
-    author: "Josh Kaufman",
-    publisher: "Portfolio",
-    publicationDate: "2010-12-30",
-    keywords: ["business"],
-    pages: 416,
-    format: "hardcover",
-    ISBN: "9781591843528",
-    language: "English",
-    thirdParty: {
-      goodreads: {
-        rating: 4.11,
-        ratingsCount: 40119,
-        reviewsCount: 1351,
-        fiveStarRatingCount: 18033,
-        oneStarRatingCount: 1090,
-      },
-    },
-  },
-  {
-    title: "Crafting Interpreters",
-    author: "Robert Nystrom",
-    publisher: "Genever Benning",
-    publicationDate: "2021-07-28",
-    keywords: [
-      "computer science",
-      "compilers",
-      "engineering",
-      "interpreters",
-      "software",
-      "engineering",
-    ],
-    pages: 865,
-    format: "paperback",
-    ISBN: "9780990582939",
-    language: "English",
-    thirdParty: {
-      goodreads: {
-        rating: 4.7,
-        ratingsCount: 253,
-        reviewsCount: 23,
-        fiveStarRatingCount: 193,
-        oneStarRatingCount: 0,
-      },
-    },
-  },
-  {
-    title: "Deep Work: Rules for Focused Success in a Distracted World",
-    author: "Cal Newport",
-    publisher: "Grand Central Publishing",
-    publicationDate: "2016-01-05",
-    edition: 1,
-    keywords: ["work", "focus", "personal development", "business"],
-    pages: 296,
-    format: "hardcover",
-    ISBN: "9781455586691",
-    language: "English",
-    thirdParty: {
-      goodreads: {
-        rating: 4.19,
-        ratingsCount: 144584,
-        reviewsCount: 11598,
-        fiveStarRatingCount: 63405,
-        oneStarRatingCount: 1808,
-      },
-    },
-    highlighted: true,
-  },
-];
+// const books = [
+//   {
+//     title: "Algorithms",
+//     author: ["Robert Sedgewick", "Kevin Wayne"],
+//     publisher: "Addison-Wesley Professional",
+//     publicationDate: "2011-03-24",
+//     edition: 4,
+//     keywords: [
+//       "computer science",
+//       "programming",
+//       "algorithms",
+//       "data structures",
+//       "java",
+//       "math",
+//       "software",
+//       "engineering",
+//     ],
+//     pages: 976,
+//     format: "hardcover",
+//     ISBN: "9780321573513",
+//     language: "English",
+//     programmingLanguage: "Java",
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.41,
+//         ratingsCount: 1733,
+//         reviewsCount: 63,
+//         fiveStarRatingCount: 976,
+//         oneStarRatingCount: 13,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: "Structure and Interpretation of Computer Programs",
+//     author: [
+//       "Harold Abelson",
+//       "Gerald Jay Sussman",
+//       "Julie Sussman (Contributor)",
+//     ],
+//     publisher: "The MIT Press",
+//     publicationDate: "2022-04-12",
+//     edition: 2,
+//     keywords: [
+//       "computer science",
+//       "programming",
+//       "javascript",
+//       "software",
+//       "engineering",
+//     ],
+//     pages: 640,
+//     format: "paperback",
+//     ISBN: "9780262543231",
+//     language: "English",
+//     programmingLanguage: "JavaScript",
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.36,
+//         ratingsCount: 14,
+//         reviewsCount: 3,
+//         fiveStarRatingCount: 8,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: "Computer Systems: A Programmer's Perspective",
+//     author: ["Randal E. Bryant", "David Richard O'Hallaron"],
+//     publisher: "Prentice Hall",
+//     publicationDate: "2002-01-01",
+//     edition: 1,
+//     keywords: [
+//       "computer science",
+//       "computer systems",
+//       "programming",
+//       "software",
+//       "C",
+//       "engineering",
+//     ],
+//     pages: 978,
+//     format: "hardcover",
+//     ISBN: "9780130340740",
+//     language: "English",
+//     programmingLanguage: "C",
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.44,
+//         ratingsCount: 1010,
+//         reviewsCount: 57,
+//         fiveStarRatingCount: 638,
+//         oneStarRatingCount: 16,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: "Operating System Concepts",
+//     author: ["Abraham Silberschatz", "Peter B. Galvin", "Greg Gagne"],
+//     publisher: "John Wiley & Sons",
+//     publicationDate: "2004-12-14",
+//     edition: 10,
+//     keywords: [
+//       "computer science",
+//       "operating systems",
+//       "programming",
+//       "software",
+//       "C",
+//       "Java",
+//       "engineering",
+//     ],
+//     pages: 921,
+//     format: "hardcover",
+//     ISBN: "9780471694663",
+//     language: "English",
+//     programmingLanguage: "C, Java",
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 3.9,
+//         ratingsCount: 2131,
+//         reviewsCount: 114,
+//         fiveStarRatingCount: 728,
+//         oneStarRatingCount: 65,
+//       },
+//     },
+//   },
+//   {
+//     title: "Engineering Mathematics",
+//     author: ["K.A. Stroud", "Dexter J. Booth"],
+//     publisher: "Palgrave",
+//     publicationDate: "2007-01-01",
+//     edition: 14,
+//     keywords: ["mathematics", "engineering"],
+//     pages: 1288,
+//     format: "paperback",
+//     ISBN: "9781403942463",
+//     language: "English",
+//     programmingLanguage: null,
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.35,
+//         ratingsCount: 370,
+//         reviewsCount: 18,
+//         fiveStarRatingCount: 211,
+//         oneStarRatingCount: 6,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: "The Personal MBA: Master the Art of Business",
+//     author: "Josh Kaufman",
+//     publisher: "Portfolio",
+//     publicationDate: "2010-12-30",
+//     keywords: ["business"],
+//     pages: 416,
+//     format: "hardcover",
+//     ISBN: "9781591843528",
+//     language: "English",
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.11,
+//         ratingsCount: 40119,
+//         reviewsCount: 1351,
+//         fiveStarRatingCount: 18033,
+//         oneStarRatingCount: 1090,
+//       },
+//     },
+//   },
+//   {
+//     title: "Crafting Interpreters",
+//     author: "Robert Nystrom",
+//     publisher: "Genever Benning",
+//     publicationDate: "2021-07-28",
+//     keywords: [
+//       "computer science",
+//       "compilers",
+//       "engineering",
+//       "interpreters",
+//       "software",
+//       "engineering",
+//     ],
+//     pages: 865,
+//     format: "paperback",
+//     ISBN: "9780990582939",
+//     language: "English",
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.7,
+//         ratingsCount: 253,
+//         reviewsCount: 23,
+//         fiveStarRatingCount: 193,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//   },
+//   {
+//     title: "Deep Work: Rules for Focused Success in a Distracted World",
+//     author: "Cal Newport",
+//     publisher: "Grand Central Publishing",
+//     publicationDate: "2016-01-05",
+//     edition: 1,
+//     keywords: ["work", "focus", "personal development", "business"],
+//     pages: 296,
+//     format: "hardcover",
+//     ISBN: "9781455586691",
+//     language: "English",
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.19,
+//         ratingsCount: 144584,
+//         reviewsCount: 11598,
+//         fiveStarRatingCount: 63405,
+//         oneStarRatingCount: 1808,
+//       },
+//     },
+//     highlighted: true,
+//   },
+// ];
 
 // Restaurant Array ///////////////////////////////////////////////////////////////////////////////////////////////////
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+// const restaurant = {
+//   name: "Classico Italiano",
+//   location: "Via Angelo Tavanti 23, Firenze, Italy",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = `20:00`,
-    adress,
-  }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`
-    );
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
-};
+//   orderDelivery: function ({
+//     starterIndex = 1,
+//     mainIndex = 0,
+//     time = `20:00`,
+//     adress,
+//   }) {
+//     console.log(
+//       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`
+//     );
+//   },
+//   orderPasta: function (ing1, ing2, ing3) {
+//     console.log(
+//       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+//     );
+//   },
+//   orderPizza: function (mainIngredient, ...otherIngredients) {
+//     console.log(mainIngredient);
+//     console.log(otherIngredients);
+//   },
+// };
+
+// Challenge #1////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+// const [players1, players2] = game.players;
+
+// console.log(players1);
+// console.log(players2);
+
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk);
+// console.log(fieldPlayers);
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+
+// const players1Final = [...players1, `Thiago`, `Coutinho`, `Perisic`];
+// console.log(players1Final);
+
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// const {
+//   odds: { team1: team1 },
+// } = game;
+// console.log(team1);
+
+// const {
+//   odds: { x: x },
+// } = game;
+// console.log(x);
+
+// const {
+//   odds: { team2: team2 },
+// } = game;
+// console.log(team2);
+
+// Can be one line
+// const {
+//   odds: { team1, x, team2 },
+// } = game;
+// console.log(team1, x, team2);
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+
+// function printGoals(...playersName) {
+//   for (let i = 0; i < playersName.length; i++) {
+//     console.log(`${i + 1} Goal: ${playersName[i]}`);
+//   }
+// }
+// printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary operator.
+
+// if (team1 < team2 && team1 < x) {
+//   console.log(`Team 1 is more likely to win with ${team1} Odds`);
+// } else if (team2 < team1 && team2 < x) {
+//   console.log(`Team 2 is more likely to win with ${team2} Odds`);
+// } else console.log(`The most likely result is a draw with ${x} Odds`);
+
+// Solution
+// team1 < team2 && console.log(`Team 1 is more likely to win with ${team1} Odds`);
+// team2 < team1 && console.log(`Team 2 is more likely to win with ${team2} Odds`);
+
 // Logical Assigment Operators/////////////////////////////////////////////////////////////////////////////////////////
 
 // const rest1 = {
@@ -305,7 +416,7 @@ const restaurant = {
 
 // Assigment #7
 
-// // 7.1
+// 7.1
 // for (let index = 0; index < books.length; index++) {
 //   books[index].edition ||= 1;
 // }
